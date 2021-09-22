34c0072e0ed045798f445175079c058ee5abd90f
Karthikeyan <28617804+Karthikeyangeo@users.noreply.github.com>
2021-09-22 15:06:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,146 @@
-// Script for html forms
-var myArray=[];
-var fname = document.getElementById("fname");
-var lname = document.getElementById("lname");
-var address = document.getElementById("address");
-var pin = document.getElementById("pin");
-var state = document.getElementById("state");
-var country = document.getElementById("country");
-// Button 
-var btn = document.querySelector(".btn");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Linking favicon -->
+    <link rel="shortcut icon" href="formfevicon.png" type="image/x-icon">
+    <!-- Linking bootstrap css files -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Linking external css files  -->
+    <link rel="stylesheet" href="style.css">
+    <title>HTML Forms</title>
+</head>
+<body>
 
-// Submit function
-btn.addEventListener("click",add_data);
-
-
-function add_data()
-{    
-    // getting value from radio button
-    var gender = document.getElementsByName("gender");
-    var gender_checked;
-    gender.forEach(x=>{
-        if(x.checked)
-        gender_checked=x.value;
-    })
-
-
-    // Getting value from checkbox
-
-    var food_arr =[];
-    var food = document.getElementsByName("food");
-    food.forEach(x =>{
-        if(x.checked)
-        {
-            food_arr.push(x.value);
-        }
-    })
-
-    // checking minimum two food options are selected
-    if(food_arr.length<2)
-    {
-        alert("Please select atleast two food items")
-    }
     
-    // saving all the values inside an object [for any future use ]
-    var myObj={
-        fname : fname.value,
-        lname : lname.value,
-        address : address.value,
-        pincode : pin.value,
-        gender : gender_checked,
-        food : food_arr,
-        state : state.value,
-        country : country.value
-
-    }
-
-    //Pushing the object to array 
+  <div class="container  justify-content-center" id="main">
+    <h2 class="text-center">Personal details</h2>
     
-    myArray.push(myObj);
-    console.log(myArray);
+    <form id="myform" action="" onsubmit="return false">
+        <!-- Name -->
+        <div class="form-group row  name">
+            <div class="col col-sm-5">
+                <label for="fname">First Name</label>
+                <input type="text" class="form-control col-6 " id="fname" placeholder="First Name">
+            </div>
+            <div class="col col-sm-5">
+                <label for="lname">Last Name</label>
+                <input type="text" class="form-control col-6 " id="lname" placeholder="Last Name">
+            </div>
+        </div>
 
-    // Resetting the form 
-    document.getElementById("myform").reset();
+        <!-- Address -->
+        <div class="form-group">
+            <label for="address">Address</label>
+            <textarea id="address" class="form-control col-7" placeholder="Enter your address here"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="pincode">Pincode</label>
+            <input type="text" id="pin" class="form-control col-3" maxlength="6" placeholder="Pincode">
+        </div>
 
-    // displaying the data in table 
+        <!-- Gender -->
+        <label for="gender" class="mr-5 mb-4">Gender</label>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="gender" id="male" value="male" checked>
+            <label class="form-check-label" for="male">
+              Male
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="gender" id="female" value="female">
+            <label class="form-check-label" for="female">
+              Female
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="gender" id="others" value="others" >
+            <label class="form-check-label" for="others">
+              Others
+            </label>
+          </div>
 
-    var tbody = document.getElementById("tbody");
-    tbody.innerHTML = "";
-    let output = "";
-    if(myArray.length>0)
-    {
-        for(let i=0;i<myArray.length;i++)
-        {
-            output += 
-            `<tr>
-            <th scope="row" id="t_sno">${i+1}</th>
-            <td id="t_fname">${myArray[i].fname}</td>
-            <td id="t_lname">${myArray[i].lname}</td>
-            <td id="t_address">${myArray[i].address}</td>
-            <td id="t_pin">${myArray[i].pincode}</td>
-            <td id="gender">${myArray[i].gender}</td>
-            <td id="t_food">${myArray[i].food}</td>
-            <td id="t_state">${myArray[i].state}</td>
-            <td id="t_country">${myArray[i].country}</td>
-          </tr>`
-        }
-    }
-    tbody.insertAdjacentHTML("beforeend",output);
+        <!-- Food -->
+        <br>
+        <label for="food" class="mr-5 mb-4">Please select food</label>
+        <div class="form-check form-check-inline ">
+            <input class="form-check-input" type="checkbox" name="food" id="Indian Cuisine" value="Indian Cuisine" >
+            <label class="form-check-label" for="Indian Cuisine">
+                Indian Cuisine
+            </label>
+          </div>
+          <div class="form-check form-check-inline ">
+            <input class="form-check-input" type="checkbox" name="food" id="Italian Cuisine" value="Italian Cuisine">
+            <label class="form-check-label" for="Italian Cuisine">
+                Italian Cuisine
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" name="food" id="Chinese Cuisine" value="Chinese Cuisine" >
+            <label class="form-check-label" for="Chinese Cuisine">
+                Chinese Cuisine
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" name="food" id="French Cuisine" value="French Cuisine" >
+            <label class="form-check-label" for="French Cuisine">
+                French Cuisine
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" name="food" id=" Thai Cuisine" value="Thai Cuisine" >
+            <label class="form-check-label" for="Thai Cuisine">
+                Thai Cuisine
+            </label>
+          </div>
+
+        <!-- state -->
+        <div class="form-group state ">
+            <label for="state">State</label>
+            <input type="text" class="form-control col-3" id="state" placeholder="State">
+        </div>
+
+        <!-- country -->
+        <div class="form-group country">
+            <label for="country">Country</label>
+            <input type="text" class="form-control col-3" id="country" placeholder="Country">
+        </div>
+        <div id="form_end">
+          <button type="submit" class="btn btn-primary submit">Submit</button>
+        </div>
+    </form>
     
-    alert("Please scroll down for the data table")
-}
+  </div>
 
+  <!-- Table  -->
+  <div class="container-fluid" id="table_div">
+    <table class="table" id="myTable" >
+      <thead class="thead-light">
+        <tr>
+          <th scope="col">S No</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Address</th>
+          <th scope="col">Pincode</th>
+          <th scope="col">Gender</th>
+          <th scope="col">Food</th>
+          <th scope="col">State</th>
+          <th scope="col">Country</th>
+        </tr>
+      </thead>
+      <tbody class="tbody" id="tbody">
+      </tbody>
+    </table>
+  </div>
+  
 
+    <!-- Linking js files -->
+    <script src="script.js"></script>
+    <!-- Linking Bootstrap JS Files -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
